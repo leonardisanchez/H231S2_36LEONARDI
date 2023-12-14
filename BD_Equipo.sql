fa7d1454dae9b90ac68af083233c9b0f318277cd
leonardisanchez <140545302+leonardisanchez@users.noreply.github.com>
2023-12-14 09:18:32 -0500

    cambio bd

--- BD_Equipo.sql
+++ BD_Equipo.sql
@@ -1,63 +1,43 @@
--- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
---
--- Host: localhost    Database: gestion_equipos_informaticos
--- ------------------------------------------------------
--- Server version	8.0.25
+-- Crear la base de datos
+CREATE DATABASE IF NOT EXISTS gestion_equipos_informaticos;
+USE gestion_equipos_informaticos;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- Crear la tabla de equipos
+CREATE TABLE IF NOT EXISTS equipos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    numero_serie VARCHAR(255),
+    marca VARCHAR(255),
+    modelo VARCHAR(255),
+    especificaciones TEXT,
+    estado_mantenimiento VARCHAR(255)
+);
 
---
--- Table structure for table `equipos`
---
+-- Listar todos los equipos
+SELECT * FROM equipos;
 
-DROP TABLE IF EXISTS `equipos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `equipos` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `numero_serie` varchar(255) DEFAULT NULL,
-  `marca` varchar(255) DEFAULT NULL,
-  `modelo` varchar(255) DEFAULT NULL,
-  `especificaciones` text,
-  `estado_mantenimiento` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+-- Agregar datos de ejemplo
+INSERT INTO equipos (numero_serie, marca, modelo, especificaciones, estado_mantenimiento)
+VALUES
+('NS001', 'HP', 'EliteBook', '8GB RAM, 256GB SSD', 'En buen estado'),
+('NS002', 'Dell', 'Latitude', '16GB RAM, 512GB SSD', 'Necesita mantenimiento');
 
---
--- Dumping data for table `equipos`
---
+-- Listar todos los equipos después de la inserción
+SELECT * FROM equipos;
 
-LOCK TABLES `equipos` WRITE;
-/*!40000 ALTER TABLE `equipos` DISABLE KEYS */;
-INSERT INTO `equipos` VALUES (1,'NS001','HP','ThinkPad','8GB RAM, 256GB SSD','En buen estado'),(2,'NS002','Dell','Latitude','16GB RAM, 512GB SSD','Necesita mantenimiento'),(3,'NS001','HP','EliteBook','8GB RAM, 256GB SSD','En buen estado'),(4,'NS002','Dell','Latitude','16GB RAM, 512GB SSD','Necesita mantenimiento');
-/*!40000 ALTER TABLE `equipos` ENABLE KEYS */;
-UNLOCK TABLES;
+-- Modificar un equipo
+UPDATE equipos
+SET modelo = 'ThinkPad'
+WHERE id = 1;
 
---
--- Dumping events for database 'gestion_equipos_informaticos'
---
+-- Listar todos los equipos después de la modificación
+SELECT * FROM equipos;
 
---
--- Dumping routines for database 'gestion_equipos_informaticos'
---
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+-- Eliminar un equipo
+DELETE FROM equipos WHERE id = 2;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+-- Listar todos los equipos después de la eliminación
+SELECT * FROM equipos;
 
--- Dump completed on 2023-12-14  9:11:25
+
+
+
